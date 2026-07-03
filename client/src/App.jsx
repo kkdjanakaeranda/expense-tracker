f91f861a5cbff1314284d818dce93154427af733
kkdjanakaeranda <erandak-ct21027@stu.kln.ac.lk>
2026-07-04 01:17:29 +0530

    titles and icons

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
 
 import Login from "./pages/Login";
 import Register from "./pages/Register";
@@ -6,6 +7,20 @@ import Dashboard from "./pages/Dashboard";
 import Settings from "./pages/Settings";
 
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    const pageTitles = {
+      "/": "Login",
+      "/register": "Create Account",
+      "/dashboard": "Dashboard",
+      "/settings": "Settings"
+    };
+    const pageTitle = pageTitles[location.pathname] || "Expense Tracker";
+
+    document.title = `${pageTitle} | Expense Tracker`;
+  }, [location.pathname]);
+
   return (
     <Routes>
       <Route path="/" element={<Login />} />
